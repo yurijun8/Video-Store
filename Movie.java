298abe6f112cfd84ba065bdd384a2829e6c44415
Yuri Junior <115372158+yurijun8@users.noreply.github.com>
2022-12-05 12:40:10 +0000

    Commit 10

--- Movie.java
+++ Movie.java
@@ -28,15 +28,15 @@ public class Movie {
     double thisAmount = 0;
     // determine amounts for each line
     switch (getPriceCode()) {
-      case Movie.REGULAR:
+      case REGULAR:
         thisAmount += 2;
         if (daysRented > 2)
           thisAmount += (daysRented - 2) * 1.5;
         break;
-      case Movie.NEW_RELEASE:
+      case NEW_RELEASE:
         thisAmount += daysRented * 3;
         break;
-      case Movie.CHILDRENS:
+      case CHILDRENS:
         thisAmount += 1.5;
         if (daysRented > 3)
           thisAmount += (daysRented - 3) * 1.5;
@@ -44,4 +44,10 @@ public class Movie {
     }
     return thisAmount;
    }
+    public int getFrequentRenterPoints(int daysRented) {
+    if ((getPriceCode() == NEW_RELEASE) &&
+        daysRented > 1)
+      return 2;
+    return 1;
+  }
 }
