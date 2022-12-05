621b7eee2eba14453e0f901aee62b92aed59cff5
Yuri Junior <115372158+yurijun8@users.noreply.github.com>
2022-12-05 13:00:39 +0000

    Commit 13

--- ChildrensPrice.java
+++ ChildrensPrice.java
@@ -1,5 +1,12 @@
 public class ChildrensPrice extends Price {
-   public int getPriceCode() {
+  public int getPriceCode() {
        return Movie.CHILDRENS;
    }
+  
+  public double getCharge(int daysRented) {
+    double result = 1.5;
+    if (daysRented > 3)
+      result += (daysRented - 3) * 1.5;
+    return result;
+  }
 }
--- NewReleasePrice.java
+++ NewReleasePrice.java
@@ -2,4 +2,7 @@ public class NewReleasePrice extends Price {
    public int getPriceCode() {
        return Movie.NEW_RELEASE;
    }
+   public double getCharge(int daysRented) {
+    return daysRented * 3;
+  }
 }
