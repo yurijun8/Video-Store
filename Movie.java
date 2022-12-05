1c5ec1d93fd3f770cedb2a3c980c45f3c30e6deb
Yuri Junior <115372158+yurijun8@users.noreply.github.com>
2022-12-05 13:02:29 +0000

    Commit 14

--- Movie.java
+++ Movie.java
@@ -41,9 +41,6 @@ public class Movie {
    }
   
     public int getFrequentRenterPoints(int daysRented) {
-    if ((getPriceCode() == NEW_RELEASE) &&
-        daysRented > 1)
-      return 2;
-    return 1;
+   return _price.getFrequentRenterPoints(daysRented);
   }
 }
