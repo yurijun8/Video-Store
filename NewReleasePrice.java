a887fb1496061e43ff626ea96256fb101c9c8343
Yuri Junior <115372158+yurijun8@users.noreply.github.com>
2022-12-05 13:03:49 +0000

    Commit 15

--- NewReleasePrice.java
+++ NewReleasePrice.java
@@ -5,4 +5,7 @@ public class NewReleasePrice extends Price {
    public double getCharge(int daysRented) {
     return daysRented * 3;
   }
+  public int getFrequentRenterPoints(int daysRented) {
+    return (daysRented > 1) ? 2 : 1;
+  }
 }
