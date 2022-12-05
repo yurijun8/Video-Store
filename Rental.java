ee6db651480550a68c8c48ab41e70448b82e7d55
Yuri Junior <115372158+yurijun8@users.noreply.github.com>
2022-12-05 12:21:35 +0000

    Commit 6

--- Rental.java
+++ Rental.java
@@ -36,5 +36,12 @@ public class Rental {
      }
      return thisAmount;
   }
+  
+   public int getFrequentRenterPoints() {
+    if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+        getDaysRented() > 1)
+      return 2;
+    return 1;
+  }
 
 }
